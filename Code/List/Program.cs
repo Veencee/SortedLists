eadd09584b865d3e7a13a6346df610b3891e5020
Veencee <110591295+Veencee@users.noreply.github.com>
2022-08-10 02:41:46 -0700

    Add files via upload

---
+++ Code/List/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace RegSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Dictionary<int, string> InecReg = new Dictionary<int, string>() 
+            {
+                {1,"John Mad"},
+                {2,"Maths Lu"},
+                {3,"Zui Zang "},
+                {4,"John Bosco"},
+                {5,"Van Damme"},
+                {6,"James Bond"},
+                {7,"Van Damme"},
+                {8,"Rock Johnson"},
+                {9,"Rambo Sly"},
+                {10,"Osama Laden"},
+                {11,"James Bond"},
+                {12,"Zui Zang "},
+                {13,"Van Damme"}
+            };
+            Console.WriteLine("                      Voters' Registeration ");
+            Console.WriteLine("                     _______________________ ");
+
+            //Go through all the elements in Dict
+            foreach (KeyValuePair<int, string> dups in InecReg)
+            {
+                Console.WriteLine(dups.Key + "  " + dups.Value);
+            }
+
+            // to find dictionary duplicate values.  
+            var duplicateValues = InecReg.GroupBy(x => x.Value).Where(x => x.Count() > 1);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("                       The Duplicates are ");
+            Console.WriteLine("                     _______________________ ");
+
+            //loop dictionary duplicate values only            
+            foreach (var names in duplicateValues)
+            {
+                Console.WriteLine(names.Key);
+            }
+        }
+    }
+}
---
+++ Code/SrtDict/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DictSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          // Create list with non-unique values
+          List<string> voters = new List<string>();
+            voters.Add("John Mad");
+            voters.Add("Maths Lu");
+            voters.Add("Zui Zang ");
+            voters.Add("John Bosco");
+            voters.Add("Van Damme");
+            voters.Add("James Bond");
+            voters.Add("Van Damme");
+            voters.Add("Rock Johnson");
+            voters.Add("Rambo Sly");
+            voters.Add("Osama Laden");
+            voters.Add("James Bond");
+            voters.Add("Zui Zang ");
+            voters.Add("Van Damme");
+
+            // Make new unique list without duplicates
+            List<string> newList = voters.Distinct().ToList();
+
+
+         Console.WriteLine("                                       Names of Voters Registered");
+         Console.WriteLine("                                      ___________________________");
+            foreach (string person in newList)
+            {
+                Console.WriteLine(person);
+
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
